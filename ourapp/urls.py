a511eff6911c316fb654ad6f3fe86e70ea7ee14b
Jake <jquinn3@uccs.edu>
2024-10-18 05:16:42 +0000

    Fixed the urls file

--- ourapp/urls.py
+++ ourapp/urls.py
@@ -6,9 +6,10 @@ urlpatterns = [
     path('', home_page, name='home_page'),
     path('search/', views.search_page, name='search_page'),
     path('search/results/', views.search_results, name='search_results'),
+    path('drink/details/<int:pk>', views.cocktailDetails.as_view(), name='Drink_detail'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('profile/', views.profile, name='profile'),
-    path('drink/details/<int:pk>', views.cocktailDetails.as_view(), name='Drink_detail'),
+    path('save_drink/<str:drink_id>/', views.save_drink, name='save_drink'),
 ]
